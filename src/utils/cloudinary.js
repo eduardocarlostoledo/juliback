277fc9635b11beec9330168ddc94763a4f15cb6a
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2025-06-09 17:42:05 -0300

    fix env de clouddinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,22 +1,21 @@
-const {v2}=require('cloudinary');
+const { v2 } = require('cloudinary');
 
-const uploadImage= async(filePath)=>{
-    return await v2.uploader.upload(filePath,{
-        //nombre de la carpeta en cloudinary
-        folder : 'Products'
-    })
-}
-
-const deleteImage= async(id)=>{
-    return await v2.uploader.destroy(id)
-}
-
+// Configuración usando variables de entorno
 v2.config({
-    cloud_name: "dgvrqx1ca",
-    api_key: "211176113828956",
-    api_secret: "UxjYWjLd2p-fZm1Tw-kqDRCAxW4",
-    secure:true
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
+const uploadImage = async (filePath) => {
+  return await v2.uploader.upload(filePath, {
+    folder: 'Products',
+  });
+};
 
-module.exports={uploadImage,deleteImage}
+const deleteImage = async (id) => {
+  return await v2.uploader.destroy(id);
+};
+
+module.exports = { uploadImage, deleteImage };
